a21182d795777bcfd1ce93b468b7f7d7581b885e
Maja Kudlicka <maja.kudlicka@gmail.com>
2020-09-17 22:25:48 +0100

    refactor: replace cars dataset with mnist dataset

--- regressions/multinomial-logistic-regression/index.js
+++ regressions/multinomial-logistic-regression/index.js
@@ -4,34 +4,39 @@ const loadCSV = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
 const plot = require('node-remote-plot');
 const _ = require('lodash');
+const mnist = require('mnist-data');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
-	shuffle: true,
-	splitTest: 50,
-	dataColumns:['horsepower', 'displacement', 'weight'],
-	labelColumns: ['mpg'],
-	converters: {
-		mpg: (value) => {
-			const mpg = parseFloat(value);
-			if (mpg < 15) return [1,0,0];
-			else if (mpg < 30) return [0,1,0];
-			else return [0,0,1];
-		}
-	}
-});
+// Wrap loading data in the function call to enable JS engine to garbage collect mnistData, which has no more
+// references to it after function executes
+function loadData() {
+	const mnistData = mnist.training(0,60000);
+	const features = mnistData.images.values.map(image => _.flatMap(image));
+	const encodedLabels = mnistData.labels.values.map(label => {
+		const row = new Array(10).fill(0);
+		row[label] = 1;
+		return row;
+	});
+	return { features, labels: encodedLabels };
+}
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-	learningRate: 0.5,
-	iterations: 100,
-	batchSize: 50
+const { features, labels } = loadData();
+
+const regression = new LogisticRegression(features, labels, {
+	learningRate: 1,
+	iterations: 20,
+	batchSize: 100
 });
 
 regression.train();
 
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+	const row = new Array(10).fill(0);
+	row[label] = 1;
+	return row;
+});
 
-// regression.predict([
-// 	[215, 440, 2.16]
-// 	]
-// ).print();
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('acc ', accuracy);
 
